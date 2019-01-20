2a2e8b594aae24a240dd90b973c0dbf4a7003ae3
Amy Peng <AmyPeng@gmail.com>
2019-01-19 16:59:52 -0800

    Added code to simple crater auto program to briong the tray down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CallistoAutoCraterSimple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CallistoAutoCraterSimple.java
@@ -65,27 +65,34 @@ public class CallistoAutoCraterSimple extends CallistoAutonomousBase {
             if (position == 1)
             //right most side when standing near the lander lift
                 {
-                myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 7, 10.0); //PERFECT
-                myEncoderDrive(Direction.STRAFE_RIGHT,DRIVE_SPEED,16,10.0);
-                myEncoderDrive(Direction.FORWARD,DRIVE_SPEED,16.5,10.0);
+                    myEncoderDrive(Direction.FORWARD, DRIVE_SPEED, 12, 20.0);
+                    myCollectionLiftDown(0.7, 1.0);
+                //myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 7, 10.0); //PERFECT
+                //myEncoderDrive(Direction.STRAFE_RIGHT,DRIVE_SPEED,16,10.0);\
+                //myEncoderDrive(Direction.FORWARD,DRIVE_SPEED,16.5,10.0);
              } else if (position == 3)
             //left most side when standing near the lander lift
             {
-                myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 11, 20.0);//PERFECT
-                myEncoderDrive(Direction.STRAFE_RIGHT,DRIVE_SPEED,53,20.0);
-                rotate(160, TURN_SPEED);
-                myEncoderDrive(Direction.BACKWARD,DRIVE_SPEED,16,10.0);
-                rotate(60,TURN_SPEED);
-                myEncoderDrive(Direction.BACKWARD,DRIVE_SPEED,10,10.0);
+                myEncoderDrive(Direction.FORWARD, DRIVE_SPEED, 12, 20.0);
+                myCollectionLiftDown(0.7, 1.0);
+                //myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 11, 20.0);//PERFECT
+                //myEncoderDrive(Direction.STRAFE_RIGHT,DRIVE_SPEED,53,20.0);
+                //rotate(160, TURN_SPEED);
+                //myEncoderDrive(Direction.BACKWARD,DRIVE_SPEED,16,10.0);
+                //rotate(60,TURN_SPEED);
+                //myEncoderDrive(Direction.BACKWARD,DRIVE_SPEED,10,10.0);
             } else // Position = 2 also default position
             //center
             {
-                myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 7, 20.0);//PERFECT
-                rotate(-180, TURN_SPEED);
-                myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED,12, 20.0);
+                myEncoderDrive(Direction.FORWARD, DRIVE_SPEED, 12, 20.0);
+                myCollectionLiftDown(0.7, 1.0);
+                //myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED, 7, 20.0);//PERFECT
+                //rotate(-180, TURN_SPEED);
+                //myEncoderDrive(Direction.BACKWARD, DRIVE_SPEED,12, 20.0);
             }
         }
         RobotLog.ii("CAL", "Exit - myDetectionRun");
+
 
     }
 }
